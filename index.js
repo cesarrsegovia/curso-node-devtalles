babfa01afaea96e9b9a49ba557040d85b3eb06d0
Cesar Segovia <cesarr.segovia27@gmail.com>
2023-04-11 17:45:05 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const { crearArchivo } = require('./helpers/multiplicar.js');
+const argv = require('./config/yargs.js')
+
+
+
+
+console.clear();
+
+
+crearArchivo(argv.b, argv.l, argv.h)
+    .then(nombreArchivo  => console.log(nombreArchivo, 'creado'))
+    .catch(err => console.log(err));
+
+
---
+++ salida/info.md
@@ -0,0 +1,2 @@
+#Informacion
+eta carpeta contiene el producto final de la aplicacion
